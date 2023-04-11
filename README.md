78527024a185e4828031e0ec333bebc32adcc17f
neerajjoshi-github <joshineeraj7037@gmail.com>
2023-04-11 08:30:07 +0530

    Initialized the app with nextjs and tailwind and created button component with cva

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a chat-app using nextjs 13 and tailwind
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Button from "<@>/components/Button";
+
+export default function Home() {
+  return (
+    <main className="text-6xl">
+      <Button>Hello</Button>
+    </main>
+  );
+}
